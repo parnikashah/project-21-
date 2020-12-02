e2f3d70ff0918df18a1bbb7119cc3ab19957274a
parnikashah <74528277+parnikashah@users.noreply.github.com>
2020-12-02 12:10:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+var bullet,wall;
+var speed,weight,thickness;
+
+
+function setup() {
+  createCanvas(1600,400);
+speed=random(223,321);
+weight=random(30,52);
+thickness=random(223,321);
+
+
+bullet=createSprite(50,200,50,10);
+bullet.velocityX=speed;
+bullet.shapecolor=color(255);
+wall=createSprite(1200,200,thickness,height/2)
+wall.shapecolor=color(80,80,80);
+
+
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  
+if(hascollided(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5*weight*speech*speech/(thickness*thickness*thickness);
+
+  if(damage>10){
+    wall.shapeColor=color(255,0,0);
+  }
+  if(damage<10){
+    wall.shapeColor=color(0,255,0);
+  }
+
+}
+
+  
+  hascollided();
+  drawSprites();
+
+  
+}
+
+function hascollided(){
+  bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true
+  }
+  return false;
+}
